eaa313901dbaae1b283543dd7463d3429e5dc1e8
Matt Martin <74032730+MatttMartin@users.noreply.github.com>
2024-04-04 22:32:13 -0400

    added error logging for socket on client side

--- frontend/src/components/messages/MessagePanel.js
+++ frontend/src/components/messages/MessagePanel.js
@@ -48,6 +48,17 @@ const MessagePanel = () => {
 			setMessages((prevMessages) => [...prevMessages, msg]);
 		});
 
+		socket.on("connect_error", (err) => {
+			// the reason of the error, for example "xhr poll error"
+			console.log(err.message);
+		
+			// some additional description, for example the status code of the initial HTTP response
+			console.log(err.description);
+		
+			// some additional context, for example the XMLHttpRequest object
+			console.log(err.context);
+		});
+
 		setSocket(newSocket);
 
 		return () => {
